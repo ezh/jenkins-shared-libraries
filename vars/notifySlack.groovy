bb108487421c63599a481faed608c5ef141dcd00
foo0x29a <thiago.nobayashi@gmail.com>
2018-05-15 16:55:20 -0300

    change channel

--- vars/notifySlack.groovy
+++ vars/notifySlack.groovy
@@ -8,7 +8,7 @@ import net.sf.json.JSONObject;
 import hudson.tasks.test.AbstractTestResultAction;
 import hudson.model.Actionable;
 
-def call(String buildStatus = 'STARTED', String channel = '#jenkins') {
+def call(String buildStatus = 'STARTED', String channel = '@nobayashi') {
 
   // buildStatus of null means successfull
   buildStatus = buildStatus ?: 'SUCCESSFUL'
