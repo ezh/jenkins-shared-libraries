97fa8f1a3b1a36aacaf2f1ec728070720ed5f7ad
foo0x29a <thiago.nobayashi@gmail.com>
2018-05-15 17:26:52 -0300

    change channel

--- vars/notifySlack.groovy
+++ vars/notifySlack.groovy
@@ -7,11 +7,11 @@ import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import hudson.model.Actionable;
 
-def call(String buildStatus = 'STARTED', String channel = '@nobayashi') {
+def call(String buildStatus = 'STARTED', String channel = '#alerts') {
 
   // buildStatus of null means successfull
   buildStatus = buildStatus ?: 'SUCCESSFUL'
-  channel = channel ?: '@nobayashi'
+  channel = channel ?: '#alerts'
 
 
   // Default values
